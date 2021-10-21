6314313f58a06d2af85055c2ae779e9f047a1b7f
Silas Brack <s174433@dtu.dk>
2021-10-21 22:46:21 +0200

    Added results from energy experiment.

--- src/experiments/run_energy.py
+++ src/experiments/run_energy.py
@@ -1,18 +1,24 @@
 from src.problems.energy import EnergyPosteriorProblem
 from pyro.distributions.transforms import planar, radial
+import numpy as np
 
 """
 We want to recreate figure 3 from Rezende et al, 2015.
 """
 
+results_folder = "results/"
+experiment_folder = "energy/"
+path = results_folder + experiment_folder
+
 epochs = 20000
-flow_samples = 256
+n_samples = 256
 for dist_name in ["U1", "U2", "U3", "U4"]:
-    for flow_type in [planar, radial]:
+    for flow_type, name in [(planar, "planar"), (radial, "radial")]:
         for n_flows in [2, 8, 32]:
             problem = EnergyPosteriorProblem(
                 dist_name=dist_name,
                 nfs=[flow_type(input_dim=2) for _ in range(n_flows)],
             )
             train_result = problem.train(epochs, plot=False)
-            flow_samples = problem.sample((flow_samples,))
+            samples = problem.sample(n_samples)
+            np.save(path + f"{dist_name}_{name}_{n_flows}.npy", samples.cpu().detach().numpy())
--- src/visualization/experiments/visualize_energy.py
+++ src/visualization/experiments/visualize_energy.py
@@ -3,36 +3,41 @@ from src.problems.energy import EnergyPosteriorProblem
 from pyro.distributions.transforms import planar, radial
 from src.visualization.visualize_distribution import plot_pdf, plot_samples
 from src.visualization.setup import setup
-import pickle
+import tikzplotlib
+import numpy as np
 setup()
 
 """
 We want to recreate figure 3 from Rezende et al, 2015.
 """
 
+plt.figure(figsize=(20, 10))
+
 index = 1
-for dist_name in ["U1", "U2", "U3", "U4"]:
+for dist_name in ["U1", "U2", "U3"]:
     dist = EnergyPosteriorProblem.get_dist(dist_name)
 
-    plt.figure(figsize=(20, 10))
     ax = plt.subplot(5, 9, index)
-    plot_pdf(dist, ax=ax).set(
+    # ax = plt.subplot(5, 7, index)
+    plot_pdf(dist, ax=ax, how="contour").set(
         title=dist_name,
         xticks=[],
         yticks=[],
     )
     index += 2
+    # index += 1
 
-    results = pickle.load()
-
-    for flow_type in [planar, radial]:
+    for flow_type, name in [(planar, "planar"), (radial, "radial")]:
         for n_flows in [2, 8, 32]:
-            flow_samples = results["flow_samples"][flow_type][n_flows]
+            results = np.load("results/energy/" + f"{dist_name}_{name}_{n_flows}.npy")
+            #flow_samples = results["flow_samples"][flow_type][n_flows]
+            flow_samples = results
 
             ax = plt.subplot(5, 9, index)
-            plot_samples(flow_samples, ax=ax)
+            # ax = plt.subplot(5, 7, index)
+            plot_samples(samples=flow_samples, ax=ax, shade=False)
             ax.set(
-                title=f"K = {n_flows}",
+                title=f"K = {n_flows}" if dist_name == "U1" else None,
                 xticks=[],
                 yticks=[],
             )
@@ -41,4 +46,6 @@ for dist_name in ["U1", "U2", "U3", "U4"]:
         index += 1
     index -= 1
 plt.tight_layout()
+# tikzplotlib.save("figures/energy/energy_grid.tex")
+plt.savefig("figures/energy/energy_grid.pdf") # backend="pgf"
 plt.show()
